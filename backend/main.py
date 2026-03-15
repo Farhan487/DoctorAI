3cdfd0a8186adcdeb5c8e0baa9c86acb9ea3be23
Farhan487 <farhanxd221@gmail.com>
2026-03-15 18:18:57 +0530

    Fix CORS syntax

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Allow react frontend to tlak to this backend 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*""],
+    allow_origins=["*"],
     # allow_origins=[
     #     "http://localhost:5173",
     #     "https://doctor-ai-jet.vercel.app"
