121af78852a6ba1aee325cf32c757cbd199a6eb4
Farhan487 <farhanxd221@gmail.com>
2026-03-15 18:09:14 +0530

    Fix CORS allow all origins

--- backend/main.py
+++ backend/main.py
@@ -10,11 +10,11 @@ app = FastAPI()
 # Allow react frontend to tlak to this backend 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:5173"],
-    allow_origins=[
-        "http://localhost:5173",
-        "https://doctor-ai-jet.vercel.app"
-    ],
+    allow_origins=["*""],
+    # allow_origins=[
+    #     "http://localhost:5173",
+    #     "https://doctor-ai-jet.vercel.app"
+    # ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
